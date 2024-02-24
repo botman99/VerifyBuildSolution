34837ba44eea2a56563c88f9508335674b59773c
Jeff Broome <jeffbroome@gmail.com>
2024-02-24 09:56:20 -0600

    Add support for Visual Studio 2022

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VerifyBuildSolution")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jeffrey Broome")]
 [assembly: AssemblyProduct("VerifyBuildSolution")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © 2019-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
--- VSPackage.cs
+++ VSPackage.cs
@@ -12,6 +12,7 @@ using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
+using System.IO;
 
 namespace VerifyBuildSolution
 {
@@ -35,7 +36,10 @@ namespace VerifyBuildSolution
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
 	[Guid(VSPackage.PackageGuidString)]
-	[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]  // We want to auto load this extension so that it doesn't need to be activated by a command (it will always be available)
+	[ProvideAutoLoad(UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
+	[ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+	[ProvideAutoLoad(UIContextGuids80.EmptySolution, PackageAutoLoadFlags.BackgroundLoad)]
+	[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string, PackageAutoLoadFlags.BackgroundLoad)]
 	public sealed class VSPackage : AsyncPackage
 	{
 		/// <summary>
@@ -44,11 +48,12 @@ namespace VerifyBuildSolution
 		public const string PackageGuidString = "2e208f6e-1d0e-436f-9b75-11b1219f32d5";
 
 		private static AsyncPackage package;
-		private static DTE2 dte2;
+		private static DTE dte;
 
-		private static CommandEvents BuildCommandEvents;
-		private static CommandEvents RebuildCommandEvents;
-		private static CommandEvents CleanCommandEvents;
+		// the CommandEvents need to be 'static' for Visual Studio 2019
+		private static CommandEvents BuildSolutionCommandEvent;
+		private static CommandEvents RebuildSolutionCommandEvent;
+		private static CommandEvents CleanSolutionCommandEvent;
 
 		#region Package Members
 
@@ -67,31 +72,28 @@ namespace VerifyBuildSolution
 
 			package = this;
 
-			dte2 = Package.GetGlobalService(typeof(DTE)) as DTE2;
+#pragma warning disable VSSDK006 // Check services exist
+			dte = await GetServiceAsync(typeof(SDTE)) as EnvDTE.DTE;
+#pragma warning restore VSSDK006 // Check services exist
 
-			Events2 evs = dte2.Events as Events2;
-			Commands cmds = dte2.Commands;
+			if (dte != null)
+			{
+				BuildSolutionCommandEvent = dte.Events.CommandEvents["{5EFC7975-14BC-11CF-9B2B-00AA00573819}", 882]; // CLSID_StandardCommandSet97, cmdidBuildSln
+				BuildSolutionCommandEvent.BeforeExecute += OnBeforeBuildSolution;
 
-			Command cmdobj = cmds.Item("Build.BuildSolution", 0);
-			BuildCommandEvents = evs.get_CommandEvents(cmdobj.Guid, cmdobj.ID);
-			BuildCommandEvents.BeforeExecute += new _dispCommandEvents_BeforeExecuteEventHandler(VerifyBuildSolution);
+				RebuildSolutionCommandEvent = dte.Events.CommandEvents["{5EFC7975-14BC-11CF-9B2B-00AA00573819}", 883]; // CLSID_StandardCommandSet97, cmdidRebuildSln
+				RebuildSolutionCommandEvent.BeforeExecute += OnBeforeRebuildSolution;
 
-			cmdobj = cmds.Item("Build.RebuildSolution", 0);
-			RebuildCommandEvents = evs.get_CommandEvents(cmdobj.Guid, cmdobj.ID);
-			RebuildCommandEvents.BeforeExecute += new _dispCommandEvents_BeforeExecuteEventHandler(VerifyRebuildSolution);
-
-			cmdobj = cmds.Item("Build.CleanSolution", 0);
-			CleanCommandEvents = evs.get_CommandEvents(cmdobj.Guid, cmdobj.ID);
-			CleanCommandEvents.BeforeExecute += new _dispCommandEvents_BeforeExecuteEventHandler(VerifyCleanSolution);
+				CleanSolutionCommandEvent = dte.Events.CommandEvents["{5EFC7975-14BC-11CF-9B2B-00AA00573819}", 885]; // CLSID_StandardCommandSet97, cmdidCleanSln
+				CleanSolutionCommandEvent.BeforeExecute += OnBeforeCleanSolution;
+			}
 		}
 
-		private static void VerifyBuildSolution(string Guid, int ID, object CustomIn, object CustomOut, ref bool CancelDefault)
+		private static void OnBeforeBuildSolution(string guid, int id, object customIn, object customOut, ref bool cancelDefault)
 		{
 			ThreadHelper.ThrowIfNotOnUIThread();
 
-			string solution_name = System.IO.Path.GetFileNameWithoutExtension(dte2.Solution.FullName).ToLower();
-
-			if ((solution_name != "ue4") && (solution_name != "ue5"))
+			if (!IsUnrealSolution())
 			{
 				return;
 			}
@@ -103,21 +105,19 @@ namespace VerifyBuildSolution
 
 			if (result == (int)VSConstants.MessageBoxResult.IDNO)  // do we want to cancel the build?
 			{
-				CancelDefault = true;
+				cancelDefault = true;
 			}
 			else
 			{
-				CancelDefault = false;
+				cancelDefault = false;
 			}
 		}
 
-		private static void VerifyRebuildSolution(string Guid, int ID, object CustomIn, object CustomOut, ref bool CancelDefault)
+		private static void OnBeforeRebuildSolution(string guid, int id, object customIn, object customOut, ref bool cancelDefault)
 		{
 			ThreadHelper.ThrowIfNotOnUIThread();
 
-			string solution_name = System.IO.Path.GetFileNameWithoutExtension(dte2.Solution.FullName).ToLower();
-
-			if ((solution_name != "ue4") && (solution_name != "ue5"))
+			if (!IsUnrealSolution())
 			{
 				return;
 			}
@@ -129,21 +129,19 @@ namespace VerifyBuildSolution
 
 			if (result == (int)VSConstants.MessageBoxResult.IDNO)  // do we want to cancel the rebuild?
 			{
-				CancelDefault = true;
+				cancelDefault = true;
 			}
 			else
 			{
-				CancelDefault = false;
+				cancelDefault = false;
 			}
 		}
 
-		private static void VerifyCleanSolution(string Guid, int ID, object CustomIn, object CustomOut, ref bool CancelDefault)
+		private static void OnBeforeCleanSolution(string guid, int id, object customIn, object customOut, ref bool cancelDefault)
 		{
 			ThreadHelper.ThrowIfNotOnUIThread();
 
-			string solution_name = System.IO.Path.GetFileNameWithoutExtension(dte2.Solution.FullName).ToLower();
-
-			if ((solution_name != "ue4") && (solution_name != "ue5"))
+			if (!IsUnrealSolution())
 			{
 				return;
 			}
@@ -155,11 +153,163 @@ namespace VerifyBuildSolution
 
 			if (result == (int)VSConstants.MessageBoxResult.IDNO)  // do we want to cancel the clean?
 			{
-				CancelDefault = true;
+				cancelDefault = true;
 			}
 			else
 			{
-				CancelDefault = false;
+				cancelDefault = false;
+			}
+		}
+
+		private static bool IsUnrealSolution()
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			IVsSolution2 solution = Package.GetGlobalService(typeof(SVsSolution)) as IVsSolution2;
+			IVsHierarchy solutionHierarchy = (IVsHierarchy)solution;
+			IVsProject solutionProject = null;
+
+			GetHierarchyInSolution(solutionHierarchy, VSConstants.VSITEMID_ROOT, ref solutionProject, "Engine", "", "",
+													out IVsHierarchy EngineHierarchy, out IVsProject EngineProject, out uint EngineItemId, out string Unused1);
+			if ((EngineHierarchy != null) && (EngineProject != null))
+			{
+				GetHierarchyInSolution(EngineHierarchy, VSConstants.VSITEMID_ROOT, ref EngineProject, "UE4", "", "",
+														out IVsHierarchy UE4Hierarchy, out IVsProject UE4Project, out uint UE4ItemId, out string Unused2);
+				if ((UE4Hierarchy != null) && (UE4Project != null))
+				{
+					return true;
+				}
+
+				GetHierarchyInSolution(EngineHierarchy, VSConstants.VSITEMID_ROOT, ref EngineProject, "UE5", "", "",
+														out IVsHierarchy UE5Hierarchy, out IVsProject UE5Project, out uint UE5ItemId, out string Unused3);
+				if ((UE5Hierarchy != null) && (UE5Project != null))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		// searches the solution for a specific project name, folder name, or file name
+		private static void GetHierarchyInSolution(IVsHierarchy hierarchy, uint itemId, ref IVsProject Project, string SearchProject, string SearchFolder, string SearchFileName, out IVsHierarchy OutHierarchy, out IVsProject OutProject, out uint OutItemId, out string OutFilename)
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			char[] InvalidChars = Path.GetInvalidPathChars();  // get characters not allowed in file paths
+
+			OutHierarchy = null;
+			OutProject = null;
+			OutItemId = VSConstants.VSITEMID_ROOT;
+			OutFilename = "";
+
+			try
+			{
+				// NOTE: If itemId == VSConstants.VSITEMID_ROOT then this hierarchy is a solution, project, or folder in the Solution Explorer
+
+				if (hierarchy == null)
+				{
+					return;
+				}
+
+				object ChildObject = null;
+
+				// Get the first visible child node
+				if (hierarchy.GetProperty(itemId, (int)__VSHPROPID.VSHPROPID_FirstVisibleChild, out ChildObject) == VSConstants.S_OK)
+				{
+					while (ChildObject != null)
+					{
+						if ((ChildObject is int) && ((uint)(int)ChildObject == VSConstants.VSITEMID_NIL))
+						{
+							break;
+						}
+
+						uint visibleChildNodeId = Convert.ToUInt32(ChildObject);
+
+						object nameObject = null;
+
+						if ((hierarchy.GetProperty(visibleChildNodeId, (int)__VSHPROPID.VSHPROPID_Name, out nameObject) == VSConstants.S_OK) && (nameObject != null))
+						{
+							if ((string)nameObject == SearchProject)
+							{
+								Guid nestedHierarchyGuid = typeof(IVsHierarchy).GUID;
+								IntPtr nestedHiearchyValue = IntPtr.Zero;
+								uint nestedItemIdValue = 0;
+
+								// see if the child node has a nested hierarchy (i.e. is it a project?, is it a folder?, etc.)...
+								if ((hierarchy.GetNestedHierarchy(visibleChildNodeId, ref nestedHierarchyGuid, out nestedHiearchyValue, out nestedItemIdValue) == VSConstants.S_OK) &&
+									(nestedHiearchyValue != IntPtr.Zero && nestedItemIdValue == VSConstants.VSITEMID_ROOT))
+								{
+									// Get the new hierarchy
+									IVsHierarchy nestedHierarchy = System.Runtime.InteropServices.Marshal.GetObjectForIUnknown(nestedHiearchyValue) as IVsHierarchy;
+									System.Runtime.InteropServices.Marshal.Release(nestedHiearchyValue);
+
+									if (nestedHierarchy != null)
+									{
+										OutHierarchy = nestedHierarchy;
+										OutProject = (IVsProject)nestedHierarchy;
+
+										return;
+									}
+								}
+							}
+							else
+							{
+								object NodeChildObject = null;
+
+								// see if this regular node has children...
+								if ((string)nameObject == SearchFolder)
+								{
+									if (hierarchy.GetProperty(visibleChildNodeId, (int)__VSHPROPID.VSHPROPID_FirstVisibleChild, out NodeChildObject) == VSConstants.S_OK)
+									{
+										if (NodeChildObject != null)
+										{
+											if ((NodeChildObject is int) && ((uint)(int)NodeChildObject != VSConstants.VSITEMID_NIL))
+											{
+												OutHierarchy = hierarchy;
+												OutProject = Project;
+												OutItemId = visibleChildNodeId;
+											}
+										}
+									}
+								}
+
+								if ((string)nameObject == SearchFileName)
+								{
+									try
+									{
+										string projectFilename = "";
+
+										if (Project.GetMkDocument(visibleChildNodeId, out projectFilename) == VSConstants.S_OK)
+										{
+											if ((projectFilename != null) && (projectFilename.Length > 0) &&
+												(!projectFilename.EndsWith("\\")) &&  // some invalid "filenames" will end with '\\'
+												(projectFilename.IndexOfAny(InvalidChars) == -1) &&
+												(projectFilename.IndexOf(":", StringComparison.OrdinalIgnoreCase) == 1))  // make sure filename is of the form: drive letter followed by colon
+											{
+												OutFilename = projectFilename;
+											}
+										}
+									}
+									catch (Exception)
+									{
+									}
+								}
+							}
+						}
+
+						ChildObject = null;
+
+						// Get the next visible sibling node
+						if (hierarchy.GetProperty(visibleChildNodeId, (int)__VSHPROPID.VSHPROPID_NextVisibleSibling, out ChildObject) != VSConstants.S_OK)
+						{
+							break;
+						}
+					}
+				}
+			}
+			catch (Exception)
+			{
 			}
 		}
 
